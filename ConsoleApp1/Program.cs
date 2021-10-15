d4269df0c6c301c54acb4705ccfc108e3d94caf2
alexarlord-boop <rezerw0.1@yandex.ru>
2021-10-15 22:13:10 +0300

    CONSOLEAPP Added a comment

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,101 +7,75 @@ using System.Text.RegularExpressions;
 
 namespace ConsoleApp1
 {
-    class UsageCounter
+    public class UsageCounter
     {
-        private readonly string _onPath;
+        // testing MSVS VCS 
+        private readonly string _inPath;
         private readonly string _outPath;
-        string Text = "";
-        string Result = "";
-        private Dictionary<string, int> _dict = new Dictionary<string, int>();
-        
+        private string Text = "";
+        private string Result = "";
+        private readonly Dictionary<string, int> _dict = new Dictionary<string, int>();
 
         public UsageCounter(string inPathArg, string outPathArg)
         {
-            _onPath = inPathArg;
+            _inPath = inPathArg;
             _outPath = outPathArg;
         }
 
-        
         public void ReadFile()
         {
-            
             try
             {
-                using FileStream stream = File.Open(this._onPath, FileMode.Open);
+                using FileStream stream = File.Open(this._inPath, FileMode.Open);
                 byte[] byteArray = new byte[stream.Length];
                 stream.Read(byteArray, 0, byteArray.Length);
                 this.Text = System.Text.Encoding.Default.GetString(byteArray);
-                Console.WriteLine("File Reading succeed.");
+                Console.WriteLine("File reading succeed.");
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            catch (Exception e) { Console.WriteLine(e.Message); }
+        }
+        public string GetText()
+        {
+            return this.Text;
         }
         public void ParseText()
         {
-
-            string[] txtLines = Regex.Split(this.Text, "[0-9\"\\.. %°“„…:;«»,\\r\\n!?\\-–XVI()]");
+            string[] textLines = Regex.Split(this.Text, "[0-9\"\\.. %°“„…:;«»,\\r\\n!?\\-–XVI()]");
             string[] lineWords;
 
-            // addition to the dict
-            foreach (string line in txtLines)
+            foreach (string line in textLines)
             {
                 lineWords = line.Split(" ");
                 foreach (string word in lineWords)
                 {
                     string cleanWord = Regex.Replace(word, "[0-9\"\\.. %°“„…:;«»,\\r\\n!?\\-–XVI()]", string.Empty);
-                    if(cleanWord.StartsWith("'") || cleanWord.EndsWith("'"))
+                    if (cleanWord.StartsWith("'") || cleanWord.EndsWith("'"))
                     {
                         cleanWord = Regex.Replace(cleanWord, "'", string.Empty);
                     }
-                    
+
                     cleanWord = cleanWord.ToLower();
-
-                    /*// checking the result
-                    Regex rgx = new Regex("[а-яa-z]");
-                    if ((cleanWord.Length > 1) & !(rgx.IsMatch(cleanWord)))
-                    {
-                        Console.WriteLine(line);
-                    }*/
-
                     if (cleanWord.Length >= 1)
                     {
-                        if (this._dict.ContainsKey(cleanWord))
-                        {
-                            this._dict[cleanWord] += 1;
-                        }
-                        else
-                        {
-                            this._dict.Add(cleanWord, 1);
-                        }
+                        if (this._dict.ContainsKey(cleanWord)) { this._dict[cleanWord] += 1; }
+                        else { this._dict.Add(cleanWord, 1); }
                     }
                 }
             }
         }
         public void CreateStat()
         {
-            if (this._dict.Count() == 0){this.Result = "";}
-            else
+            List<string> lstOfLines = new List<string>();
+            int maxLenght = 5 + (from k in this._dict.Keys orderby k.Length descending select k).FirstOrDefault().Length;
+            var sortedDictByValue = from pair in this._dict orderby pair.Value descending select pair;
+            foreach (KeyValuePair<string, int> pair in sortedDictByValue)
             {
-                string row = "";
-                int maxLenght = 5 + (from k in this._dict.Keys orderby k.Length descending select k).FirstOrDefault().Length;
-                List<string> lst = new List<string>();
-
-                var sortedDictByValue = from pair in this._dict orderby pair.Value descending select pair;
-                foreach (KeyValuePair<string, int> pair in sortedDictByValue)
-                {
-                    int keyLenght = pair.Key.Length;
-                    int valLenght = pair.Value.ToString().Length;
-                    int spaceLenght = maxLenght - (keyLenght + valLenght);
-                    row = pair.Key + new string(' ', spaceLenght) + pair.Value.ToString();
-                    lst.Add(row);
-                }
-                this.Result = string.Join("\n", lst);
+                int spaceLenght = maxLenght - (pair.Key.Length + pair.Value.ToString().Length);
+                lstOfLines.Add(pair.Key + new string(' ', spaceLenght) + pair.Value.ToString());
             }
-        }
+            this.Result = string.Join("\n", lstOfLines);
 
+        }
         public void WriteFile()
         {
             try
@@ -109,37 +83,46 @@ namespace ConsoleApp1
                 using FileStream outStream = File.Open(this._outPath, FileMode.Create);
                 byte[] byteArray = System.Text.Encoding.Default.GetBytes(this.Result);
                 outStream.Write(byteArray, 0, byteArray.Length);
+                Console.WriteLine("File writing succeed.");
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            catch (Exception e) { Console.WriteLine(e.Message); }
         }
-        
     }
     class Program
     {
         static void Main(string[] args)
         {
-            // creating UsageCounter obj
-            //string inPath = "C:\\Users\\Александр\\OneDrive\\Документы\\book.txt";
-            //string outPath = "C:\\Users\\Александр\\OneDrive\\Документы\\word_stat.txt";
-            Console.WriteLine("Input path: ");
-            string inPath = Console.ReadLine();
-            Console.WriteLine("Output path: ");
-            string outPath = Console.ReadLine();
+            string inPath = "";
+            string outPath = "";
+            if (args.Length == 2)
+            {
+                inPath = args[0];
+                outPath = args[1];
+            }
+            else if(args.Length != 0)
+            {
+                Console.WriteLine("Incorrect arguments.");
+            }
+            else
+            {
+                Console.Write("Input path:  ");
+                inPath = Console.ReadLine();
+                Console.Write("Output path: ");
+                outPath = Console.ReadLine();
+            }
 
             UsageCounter counter = new UsageCounter(inPath, outPath);
-            
             counter.ReadFile();
-
-            counter.ParseText();
-
-            counter.CreateStat();
-          
-            counter.WriteFile();
-
+            if (counter.GetText().Length == 0)
+            {
+                Console.WriteLine("Incorrect input data.");
+            }
+            else
+            {
+                counter.ParseText();
+                counter.CreateStat();
+                counter.WriteFile();
+            }
         }
     }
 }
-
