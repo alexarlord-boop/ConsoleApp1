30798dba117de056a93200f8f5a784d8fa68288e
alexarlord-boop <rezerw0.1@yandex.ru>
2021-10-20 16:54:08 +0300

    Added abstract class instance creation via reflection

---
+++ AbstractCLass/AbstractInstance.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+
+namespace AbstractCLass
+{
+
+    abstract class Class1
+    {
+        public int number;
+        public string str;
+
+        public Class1(int n, string s)
+        {
+            this.number = n;
+            this.str = s;
+        }
+    }
+
+    class AbstractInstance
+    {
+        static void Main(string[] args)
+        {
+            Type t = typeof(Class1);
+            Console.WriteLine(t.Name);
+            //var c = t.Assembly.CreateInstance(t.Name);
+
+            ConstructorInfo ci = t.GetConstructor(new Type[] { typeof(int), typeof(string) });
+            //object instance = ci.Invoke(new object[] { 10, "str"});
+            var innstance = (Class1)Activator.CreateInstance(t, 1, "str");
+
+            
+            
+        }
+    }
+}
