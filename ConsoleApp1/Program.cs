58f66bd206ab9a1cf23e0b1ab2a6527d98d6a964
alexarlord-boop <rezerw0.1@yandex.ru>
2021-10-23 15:27:52 +0300

    Refactored code

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -18,68 +18,6 @@ namespace ConsoleApp1
             string fileData = "";
             string result = "";
 
-            /*if (args.Length == 2)
-            {
-                inPath = args[0];
-                outPath = args[1];
-            }
-            else if (args.Length != 0)
-            {
-                Console.WriteLine("Incorrect arguments.");
-            }
-            else
-            {
-                Console.Write("Input path:  ");
-                inPath = Console.ReadLine();
-                Console.Write("Output path: ");
-                outPath = Console.ReadLine();
-            }*/
-
-            /* // reading data
-             fileData = ReadFile(inPath);
-             string[] arg = new string[] { fileData };
-
-             if (fileData.Length == 0)
-             {
-                 Console.WriteLine("Incorrect input data.");
-             }
-             else
-             {
-                 //creating object
-                 var t = typeof(UsageCounter);
-                 var counter = (UsageCounter)Activator.CreateInstance(t);
-
-                 //getting private method of the instance
-                 MethodInfo createStat = counter.GetType().GetMethod("CreateStat", BindingFlags.NonPublic | BindingFlags.Instance);
-
-                 Console.WriteLine("Regular   Thread");
-                 for (int i = 0; i < 50; i++)
-                 {
-                     //getting the result string via standard method
-                     stopwatch.Start();
-                     result = (string)createStat.Invoke(counter, arg);
-                     stopwatch.Stop();
-                     string res1 = stopwatch.ElapsedMilliseconds.ToString();
-                     stopwatch.Reset();
-
-                     //writing data
-                     WriteFile(result, outPath);
-
-
-                     //getting the result string via FOREACH thread method
-                     stopwatch.Start();
-                     result = counter.ThreadCreateStat(fileData);
-                     stopwatch.Stop();
-                     string res2 = stopwatch.ElapsedMilliseconds.ToString();
-                     stopwatch.Reset();
-
-                     //writing new data
-                     WriteFile(result, outPath);
-
-                     Console.WriteLine(res1 + "       " + res2);
-                 }
-             }*/
-
 
             /*-------------------------WEB SERVICE PART-------------------------*/
             //1. getting text from user <- console:ReadLine | CLIENT
